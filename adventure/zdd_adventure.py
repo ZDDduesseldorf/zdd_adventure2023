ebfed747ddee5360abdd267d2af03db315bae009
florian-huber <florian.huber@hs-duesseldorf.de>
2023-09-15 14:07:49 +0200

    initial code

---
+++ adventure/zdd_adventure.py
@@ -0,0 +1,143 @@
+class Item:
+    def __init__(self, name, description, movable=False):
+        """Construct a new Item
+        
+        Parameters
+        ----------
+        name : str
+            Name of the item
+        description : str
+            Description of the item.
+        movable : bool
+            Determines whether the item is movable, i.e., whether it can be added to the inventory.
+            Defaults to False.
+        """
+        self.name = name
+        self.description = description
+        self.movable = movable
+
+    def describe(self):
+        print(self.description)
+
+    def is_movable(self, items, other_item_required=None):
+        """Returns True if item can be added to the inventory."""
+        if not self.movable:
+            return False
+        if not other_item_required:
+            return True
+        if other_item_required and other_item_required in items:
+            return True
+        return False
+
+class Room:
+    def __init__(self, name, description, items=None):
+        """Create a Room
+
+        Parameters
+        ----------
+        name : str
+            Name of the room.
+        description : str
+            Short description of the room.
+        items : Optional
+            Additional items to (Item objects) that will be visible to the user upon inspection of the room.
+        """
+        self.name = name
+        self.description = description
+        self.visited = 0
+        if isinstance(items, list):
+            self.items = items
+        else:
+            self.items = [items]
+
+    def enter_room(self, user_items):
+        """Main method of Room class."""
+        self.visited += 1
+        print(40 * "-")
+
+        # Run whatever happens in this room:
+        user_items = self.run_story(user_items)
+
+        while True:
+            action = input(">> 'leave' to exit the room, 'inspect' to look around: ").lower()
+            if action == "leave":
+                print("You leave the room...")
+                return user_items
+            elif action == "inspect":
+                user_items = self.show_items(user_items)
+            else:
+                print("Invalid command!")
+
+    def run_story(self, user_items):
+        """Customizable method where students can add functionality."""
+        print(f"You've entered the {self.name} {self.visited} times.")
+        # By default, this method doesn't do much, but students can enhance this method in their custom rooms.
+
+        return user_items
+
+    def show_items(self, user_items):
+        """Show all items in the room and ask for addition to the inventory (if item is movable)."""
+        if len(self.items) > 0:
+            print("In here you find...")
+        else:
+            print("There is nothing of particular interest...")
+            return user_items
+
+        for item in self.items:
+            item.describe()
+            if item.is_movable(self.items):
+                while True:
+                    action = input(f"Do you want to take {item.name}? (yes/no): ").lower()
+                    if action == "yes":
+                        user_items.append(item)
+                        self.items.remove(item)
+                        print(f"You've taken the {item.name}.")
+                        break
+                    if action == "no":
+                        print(f"You leave the {item.name} where it is.")
+                        break
+                    print("Please answer with yes or no!")
+        return user_items
+
+    def get_detail(self):
+        return f"You are in the {self.name}. {self.description}"
+
+
+class Floor:
+    def __init__(self, name, description):
+        """Create a new Floor.
+
+        Parameters
+        ----------
+        name : str
+            Name of the floor.
+        description : str
+            Short description of the floor.
+        """
+        self.name = name
+        self.description = description
+        self.rooms = {}
+        self.connected_floors = {}
+
+    def add_room(self, direction, room):
+        """Add a room to the floor."""
+        self.rooms[direction] = room
+
+    def get_room(self, direction):
+        """Get room in a specific direction."""
+        return self.rooms.get(direction, None)
+
+    def add_connection(self, direction, floor):
+        """Define connections between floors."""
+        self.connected_floors[direction] = floor
+
+    def get_floor_in_direction(self, direction):
+        return self.connected_floors.get(direction, None)
+
+    def get_orientation(self):
+        details = f"You are on the {self.name}. {self.description}\n"
+        for direction, room in self.rooms.items():
+            details += f"\nType 'enter {direction}' to go to {room.name}."
+        for direction, floor in self.connected_floors.items():
+            details += f"\nType 'go {direction}' to go to {floor.name}."
+        return details
