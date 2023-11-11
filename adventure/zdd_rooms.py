727f8cb1c2142f40f9e9f25c00cd9f27c12d962e
Jonah Gräfe <jonah.greafe@gmail.com>
2023-11-11 16:01:51 +0100

    Added comments

--- adventure/zdd_rooms.py
+++ adventure/zdd_rooms.py
@@ -17,42 +17,56 @@ class ToiletCellar(Room):
 
 class PigeonHouse(Room):
     def run_story(self, user_items):
+        # Introduction and initial choices
         print("You walk through an old, slightly broken door.\n"
               "Inside, there are no pigeons.\n"
               "You see an old window, do you want to open it?")
         
+        # First Choice: Open or Don't Open the Window
         while True:
             choice = input("Type 'open window' or 'do not open window'\n"
                            "What do you want to do?: ")
             if choice == "open window":
+                # Outcome: User chooses to open the window
                 print("The window opens, and you hear something fall to the floor behind you. You go there and see a broken egg and a baby pigeon on the floor.\n")
                 break
             elif choice == "do not open window":
+                # Outcome: User decides not to open the window
                 print("You decide not to open the window and look around the room. You see an abandoned pigeon's nest in a corner.\n")
                 
+                # Second Choice: Inspect the Nest or Don't Inspect
                 while True:
                     inspect_choice = input("Type 'inspect nest' or 'do not inspect nest'\n"
                                            "What do you want to do?: ")
                     if inspect_choice == "inspect nest":
+                        # Outcome: User decides to inspect the nest
                         print("You inspect the nest and see an egg. Suddenly the egg moves, and a baby pigeon hatches from it.\n")
                         break
                     elif inspect_choice == "do not inspect nest":
+                        # Outcome: User decides not to inspect the nest
                         print("But suddenly you hear a soft 'gurrrrrr' and look into the nest. You see a little baby pigeon that has obviously just hatched.\n")
                         break
                     else:
+                        # Handling invalid inputs
                         print("Invalid input. Please try again.")
             else:
+                # Handling invalid inputs
                 print("Invalid input. Please try again.")
             
-            break
+            break  # Exiting the first while loop
+
+        # Third Choice: Ask the pigeon to be a companion
         while True:
             choice = input("Do you want to ask the pigeon to be your companion? (yes/no): ")
             if choice == "yes":
+                # Outcome: User decides to make the pigeon their companion
                 print("It answers 'gurrr!' which (of course) means yes, so you put it into your bag.")
                 break
             elif choice == "no":
+                # Outcome: User decides not to make the pigeon their companion
                 print("The pigeon looks at you with its big round eyes and says 'gurrr!'. Maybe you should think about it again.")
             else: 
+                # Handling invalid inputs
                 print("Invalid input. Please try again.")
             
         return user_items
