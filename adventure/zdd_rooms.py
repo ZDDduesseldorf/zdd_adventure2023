3fcac6e698a700900efcdb9053e335d342a085ab
honnigmelone <nikita.rosik@study.hs-duesseldorf.de>
2023-11-12 21:37:52 +0100

    added story

--- adventure/zdd_rooms.py
+++ adventure/zdd_rooms.py
@@ -13,11 +13,51 @@ class ToiletCellar(Room):
         return user_items
     
 
+
+
 class BubbleteaShop(Room):
+
+    list_boba = ["kiwi", "mango", "coffee"]
+    list_tea = ["crazygrape", "tangomango", "limeblossom"]
+
     def run_story(self, user_items):
+        print("You enter the Bubbletea Shop and its incredibly cute\n",
+              "In there is an old lady, a well-known face that was serving in the Cafeteria before.\n",
+              "She starts talking to you...\n\n",
+              "Hey lovely, would you like to order a Bubbletea today?\n")
 
-        pass
+        while True:
+            input_choice = input("Type 'yes' if you want to order and 'no' if not: ")
 
+            if input_choice == "no":
+                print("What a pity! Come back if you change your mind!\n")
+                return user_items
+
+            elif input_choice == "yes":
+                print("Great, we have 3 types of tea and 3 types of boba!\n",
+                      "What flavour would you like to have for your Tea?\n",
+                      "We have Crazy-Grape, Tango-Mango and Lime-Blossom\n")
+
+                input_choice_tea = input("Type 'crazygrape','tangomango' or 'limeblossom': ")
+
+                if input_choice_tea not in self.list_tea:
+                    print("Invalid choice, try again")
+                    continue
+
+                print(f"{input_choice_tea} is an interesting choice. Now tell me, what boba do you want \n",
+                      "We have Coffee, Kiwi, and Mango")
+
+                input_choice_boba = input("Type 'coffee', 'kiwi', or 'mango': ")
+
+                if input_choice_boba not in self.list_boba:
+                    print("Invalid choice, try again")
+                    continue
+
+                print(f"Here you go, enjoy your {input_choice_tea} tea with {input_choice_boba}!")
+                
+                return user_items
+
+                    
 ## ----------------------------------------------------------------
 ## List here all rooms
 
