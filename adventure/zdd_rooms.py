072b7923d6bcc4c376f4c24fdc02992a7d90ac6f
Josh <Joschua.Schramm@yahoo.de>
2023-11-14 15:55:20 +0100

    added enchanting room + sharpness V book

--- adventure/zdd_rooms.py
+++ adventure/zdd_rooms.py
@@ -1,5 +1,5 @@
 """This is to keep all special rooms of the ZDD."""
-from main_classes import Room
+from main_classes import Room, Item
 
 
 class ToiletCellar(Room):
@@ -12,16 +12,31 @@ class ToiletCellar(Room):
             return [x for x in user_items if x.name != "old book"]
         return user_items
 
+class EnchantingTable(Room):
+    def run_story(self, user_items):
+        print("You enter a dimly lit room, filled with an eerie, ominous hum.")
+        print("In the center, an Enchanting Table glows faintly, surrounded by rows of ancient bookshelves.")
+
+        sharpness_V = Item("Sharpness V", "sharper than rihanna", movable=True)
+        choice = input("You see a book surrounded by a mystical glow lying on one of the bookshelves. Do you want to pick it up? (yes/no)\n").lower()
+        if choice == "yes":
+            return user_items.append(sharpness_V)
+        else:
+            print("As you wish")
+        return user_items
+
+    
 ## ----------------------------------------------------------------
 ## List here all rooms
+
 
 toilet_cellar = ToiletCellar("toilet", "Yes, even the cellar has a toilet.")
 # Add your room instance here, similar to the example below:
 # my_room = MyRoom("room_name", "room_description")
-
+enchanter = EnchantingTable("enchanting", "Luckily you just hit 30 levels!")
 
 ALL_ROOMS = {
-    "toilet_cellar": toilet_cellar
-    # Add your room key-value pairs here:
+    "toilet_cellar": toilet_cellar,
+    "enchanter": enchanter
     # "my_room_key": my_room
 }
